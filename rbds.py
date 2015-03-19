2c9d24ef92c0dc59d6bccf7ec93a99bf8ec1dc76
Roux Heyns <rheyns@gmail.com>
2015-03-18 21:43:45 -0700

    Add some example code for dealing with POST requests from RockBlock

--- rbds.py
+++ rbds.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 
-def main():
-  print "Hello world!"
+from flask import Flask
+import requests
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route('/messages', methods=['POST'])
+def messages():
+  """Process messages from RockBlock"""
+  # Do stuff here
+  # Now send confirmation back
+  r = requests.get('http://some-server/path')
+  if r.status_code != 200:
+    # Handle some sort of failure, since we didn't get the 200 OK code
+
+def main():
+  app.run(host='0.0.0.0', port=5050)
+
+if __name__ == '__main__':
   main()
