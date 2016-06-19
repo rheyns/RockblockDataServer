204c4de44c44566ae1fd18c1c5adf68aee5e18fd
pandora <fakey@fakerson.org>
2016-06-18 23:25:13 -0400

    added hello world part to rbds.py server code

--- rbds/rbds.py
+++ rbds/rbds.py
@@ -3,7 +3,7 @@
 from flask import Flask
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/messages', methods=['POST'])
 def messages():
@@ -13,6 +13,12 @@ def messages():
   r = requests.get('http://some-server/path')
   if r.status_code != 200:
     # Handle some sort of failure, since we didn't get the 200 OK code
+    pass
+
+@app.route('/', methods=['GET'])
+def homepage():
+  """Serve up a default homepage"""
+  return 'Hello World, and Alex says hi too'
 
 def main():
   app.run(host='0.0.0.0', port=5050)
